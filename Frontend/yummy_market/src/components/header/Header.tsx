ff9431dee7a8ab974a6d869dab5e392809dad3c1
eunbyeol <jsily2288@naver.com>
2025-04-20 01:52:14 +0900

    로그인페이지 퍼블리싱 중

--- Frontend/yummy_market/src/components/header/Header.tsx
+++ Frontend/yummy_market/src/components/header/Header.tsx
@@ -2,14 +2,12 @@ import Logo from "./Logo";
 import Nav from "./Nav";
 import '../../styles/header.css'
 
-
-
 export default function Header(){
 
   return(
       <>
-      <header className="hd  w-100">
-        <div className="center row-flex-center between">
+      <header className="hd ">
+        <div className="center  ">
           <Logo />
           <Nav />
         </div>
--- Frontend/yummy_market/src/components/header/Logo.tsx
+++ Frontend/yummy_market/src/components/header/Logo.tsx
@@ -1,12 +1,39 @@
+import { User,ShoppingCart } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Logo(){
+
+
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/login');
+  };
+
   return(
       <>
-        <h1>
-          <a href="" className="montserrat-extra-bold fs-30 ">
-            <span className="logo_first">Y</span>ummy <br/>
-            Market
+        <h1 className="w-100 row-flex align-end between mb-2">
+          <a href="/" className="montserrat-extra-bold fs-30 ">
+       Yummy
+            <br/>
+    Market
           </a>
+          <div className="row-flex-center gap-10 hd_nav">
+            <div className="row-flex-center gap-3">
+              <div className="user_icon pointer" onClick={handleClick}>
+                <User />
+              </div>
+              {/*<a href="" className="gmarket-medium fs-18 ">로그인</a>*/}
+            </div>
+            <div className="row-flex-center gap-3">
+              <div className="cart_icon pointer">
+              <ShoppingCart />
+              </div>
+              {/*<a href="" className="gmarket-medium fs-18 ">장바구니</a>*/}
+
+            </div>
+          </div>
+
         </h1>
       </>
   )

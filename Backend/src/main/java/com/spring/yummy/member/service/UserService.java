8aab53c1ea46b9baebeb7dff6f00ef0e70aacfa4
saven2762 <https://github.com/seven2762>
2025-04-21 23:14:41 +0900

    [feature][login-with-jwt] 패스워드 암호화

--- Backend/src/main/java/com/spring/yummy/member/service/UserService.java
+++ Backend/src/main/java/com/spring/yummy/member/service/UserService.java
@@ -4,6 +4,7 @@ import com.spring.yummy.member.dto.request.UserCreateRequestDTO;
 import com.spring.yummy.member.entity.User;
 import com.spring.yummy.member.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,10 +12,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public void CreateUser(UserCreateRequestDTO requestDTO) {
         userRepository.save(
-            User.create(requestDTO.username(), requestDTO.password(), requestDTO.email(),
+            User.create(requestDTO.username(), bCryptPasswordEncoder.encode(requestDTO.password()), requestDTO.email(),
                 requestDTO.phone()));
     }
 }

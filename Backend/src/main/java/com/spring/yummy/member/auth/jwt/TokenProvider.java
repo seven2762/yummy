b4abed6eee2f948fb433e7cf068afead681a0e47
saven2762 <https://github.com/seven2762>
2025-04-22 23:42:30 +0900

    [feature][login-with-jwt] docker compose 파일 수정 및 application.yml 수정

--- Backend/src/main/java/com/spring/yummy/member/auth/jwt/TokenProvider.java
+++ Backend/src/main/java/com/spring/yummy/member/auth/jwt/TokenProvider.java
@@ -89,16 +89,12 @@ public class TokenProvider implements InitializingBean {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
         } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
-            
             logger.info("잘못된 JWT 서명입니다.");
         } catch (ExpiredJwtException e) {
-            
             logger.info("만료된 JWT 토큰입니다.");
         } catch (UnsupportedJwtException e) {
-            
             logger.info("지원되지 않는 JWT 토큰입니다.");
         } catch (IllegalArgumentException e) {
-            
             logger.info("JWT 토큰이 잘못되었습니다.");
         }
         return false;

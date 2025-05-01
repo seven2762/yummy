cda0a5fa9bbd004874cf0d24987aaf9d34376aad
saven2762 <https://github.com/seven2762>
2025-05-01 22:11:23 +0900

    [feature][login-with-jwt] 주소 추가 컨트롤러 추가

--- Backend/src/main/java/com/spring/yummy/member/controller/UserController.java
+++ Backend/src/main/java/com/spring/yummy/member/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.spring.yummy.member.controller;
 
 import com.spring.yummy.global.dto.ApiResDto;
+import com.spring.yummy.member.dto.request.AddressRequestDTO;
 import com.spring.yummy.member.dto.request.UserCreateRequestDTO;
+import com.spring.yummy.member.dto.response.AddressRes;
+import com.spring.yummy.member.entity.Address;
 import com.spring.yummy.member.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +24,26 @@ public class UserController {
 
 
     @PostMapping("/signUp")
-    public ResponseEntity<ApiResDto> createUser(UserCreateRequestDTO requestDTO) {
+    public ResponseEntity<ApiResDto> createUser(@RequestBody UserCreateRequestDTO requestDTO) {
 
-        userService.CreateUser(requestDTO);
+        userService.createUser(requestDTO);
 
         return ResponseEntity.ok().body(new ApiResDto("회원 가입이 성공적으로 완료되었습니다", HttpStatus.OK.value()));
 
+    }
+
+    @PostMapping("/{userId}/address")
+    public ResponseEntity<ApiResDto> addUserAddress(
+        @PathVariable("userId") Long userId,
+        @RequestBody AddressRequestDTO requestDTO) {
+
+        AddressRes newAddress = userService.addUserAddress(userId, requestDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(new ApiResDto(
+                "주소가 성공적으로 추가되었습니다",
+                HttpStatus.CREATED.value(),
+                newAddress
+            ));
     }
 }

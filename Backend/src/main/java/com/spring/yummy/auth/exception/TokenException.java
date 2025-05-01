aa4d8db79652564946b241af0dc41f0b6483719f
saven2762 <https://github.com/seven2762>
2025-05-01 22:16:40 +0900

    [feature][login-with-jwt] 토큰 예외 클래스 추가

---
+++ Backend/src/main/java/com/spring/yummy/auth/exception/TokenException.java
@@ -0,0 +1,10 @@
+package com.spring.yummy.auth.exception;
+
+public class TokenException extends RuntimeException {
+
+
+    public TokenException(String message) {
+        super(message);
+    }
+
+}

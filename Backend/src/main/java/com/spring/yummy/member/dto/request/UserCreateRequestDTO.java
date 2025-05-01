86749260cc4778f8c2c8d9bcceebf54c6be008e9
saven2762 <https://github.com/seven2762>
2025-05-01 22:11:41 +0900

    [feature][login-with-jwt] 주소 추가 컨트롤러 추가

--- Backend/src/main/java/com/spring/yummy/member/dto/request/UserCreateRequestDTO.java
+++ Backend/src/main/java/com/spring/yummy/member/dto/request/UserCreateRequestDTO.java
@@ -1,8 +1,13 @@
 package com.spring.yummy.member.dto.request;
 
-
 import com.spring.yummy.member.entity.Role;
 
-public record UserCreateRequestDTO(String email, String username, String password, String phone, Role role) {
-
-}
+public record UserCreateRequestDTO(
+    String email,
+    String username,
+    String password,
+    String phone,
+    Role role,
+    AddressRequestDTO address
+)
+{}

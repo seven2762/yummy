3ed1364b40ef444eeca2dcf7636ad25155770dbc
saven2762 <https://github.com/seven2762>
2025-05-05 02:25:33 +0900

    application-docker.yml redisconfig 수정

--- Backend/src/main/java/com/spring/yummy/global/config/RedisConfig.java
+++ Backend/src/main/java/com/spring/yummy/global/config/RedisConfig.java
@@ -18,7 +18,7 @@ public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${spring.data.redis.port")
+    @Value("${spring.data.redis.port}")
     private String redisPort;
 
     @Value("${spring.data.redis.password}")
